35c6f624e160249e70b1fd53eaa852f95e0478e5
Jacob Carse <jacob.carse@gmail.com>
2015-11-08 13:41:46 +0000

    Counts Circles
    Work on more accurately erode and dilate the images as well as de noise

--- Assignment.m
+++ Assignment.m
@@ -10,7 +10,9 @@ function [ output_image ] = Assignment( input )
     dilated_image = Dilate(hole_image,6);
     output_image = Erode(dilated_image,6);
     output_image = output_image - hole_image;
-    %output_image = Dilate(output_image, 2);
-    %output_image = Erode(output_image, 3);
+    output_image = Erode(output_image, 1);
+    output_image = Dilate(output_image, 1);
+    count = size(unique(CCLabaling(output_image)));
+    disp(count(1)-1);
     
 end

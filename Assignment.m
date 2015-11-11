b3ebe27b17d3cb1b3fd09a50464a457d21e88751
Jacob Carse <jacob.carse@gmail.com>
2015-11-11 19:04:27 +0000

    Changes to Filter

--- Assignment.m
+++ Assignment.m
@@ -2,18 +2,18 @@ function [ output_image ] = Assignment( input )
 
     input_image = imread(input);
     input_image = rgb2gray(input_image);
-    i = Medium(input_image, 5);
+    i = imfilter(input_image, fspecial('average', [2,2]));
+    %i = Medium(i, 2);
     level = graythresh(i);
     denoised_image = im2bw(i,level);
     lables = CCLabaling(denoised_image);
     hole_image = SelectValue(lables);
-    se = strel('diamond',10);
+    se = strel('disk',15);
     dilated_image = CirDilate(hole_image,se);
     output_image = CirErode(dilated_image,se);
     output_image = output_image - hole_image;
-    se = strel('diamond',1);
-    output_image = CirErode(output_image, se);
-    output_image = CirDilate(output_image, se);
+    output_image = CirErode(output_image, strel('disk',1));
+    output_image = CirDilate(output_image, strel('disk',5));
     count = size(unique(CCLabaling(output_image)));
     disp(count(1)-1);
     

aa4be2e01c4b0082c7e5b303f63730c9036d7586
Jacob Carse <jacob.carse@gmail.com>
2015-11-15 19:22:05 +0000

    Formatted and Added semi circle detection

--- Assignment.m
+++ Assignment.m
@@ -1,12 +1,23 @@
 function [ output_image ] = Assignment( input )
 
+    %Task 1
     input_image = imread(input);
     gray_image = rgb2gray(input_image);
-    i = Medium(gray_image, 2);
+    i = Medium(gray_image, 3);
     level = graythresh(i);
-    denoised_image = im2bw(i,level);
+    denoised_image = im2bw(i,level+0.05);
+    
+    %Task 2
     lables = CCLabaling(denoised_image);
-    hole_image = SelectValue(lables);
+    thing = SelectValue(lables, 1);
+    thing = CirDilate(thing, strel('disk',1));
+    imshow(thing);
+    denoised_image = denoised_image + thing;
+    figure, imshow(denoised_image);
+    lables = CCLabaling(denoised_image);
+    hole_image = SelectValue(lables,2) + SelectValue(lables,3);
+    
+    %Task 3
     se = strel('disk',15);
     dilated_image = CirDilate(hole_image,se);
     circle_image = CirErode(dilated_image,se);
@@ -15,8 +26,10 @@ function [ output_image ] = Assignment( input )
     circle_image = CirDilate(circle_image, strel('disk',1));
     count = size(unique(CCLabaling(circle_image)));
     disp(count(1)-1);
-    edge_image = edge(circle_image);
     
+    %Task 4
+    edge_image = edge(circle_image);
     output_image = input_image + Binary2green(edge_image);
+    figure;
     
 end
--- SelectValue.m
+++ SelectValue.m
@@ -1,15 +1,14 @@
-function [ output_image ] = SelectValue( input_image )
+function [ output_image ] = SelectValue( input_image, value )
 
     [n,m]=size(input_image);
     values = unique(input_image);
     frequency = [values,histc(input_image(:),values)];
     frequency = sortrows(frequency,2);
-    value1 = frequency(length(frequency)-2,1);
-    value2 = frequency(length(frequency)-3,1);
+    value1 = frequency(length(frequency)-value,1);
     
     for i=1:n
         for j=1:m
-            if input_image(i,j)==value1 || input_image(i,j)==value2
+            if input_image(i,j)==value1
                 input_image(i,j)=1;
             else
                 input_image(i,j)=0;

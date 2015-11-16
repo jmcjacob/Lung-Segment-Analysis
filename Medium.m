53e1a1ccaa1d78a083c20d72f687b6ff59ed9d1e
Jacob Carse <jacob.carse@gmail.com>
2015-11-16 16:22:28 +0000

    More commenting on images

--- Medium.m
+++ Medium.m
@@ -1,11 +1,14 @@
 function [ output_image ] = Medium( input_image, filter_size )
+%MEDIUM applies a Medium filter to an image of a certain size.
 
+    % Sets up the filter, padds the image and sets up the image.
     [m,n]=size(input_image);
-    output_image = ones(m,n);
+    output_image = uint8(ones(m,n));
     padding = floor(filter_size/2);
     input_image=padarray(input_image,[padding,padding],'symmetric');
     window = zeros(1,filter_size*filter_size);
     
+    % The Goes through the image and filter applying the filter.
     for x=padding+1:m+padding
        for y=padding+1:n+padding
            i=1;
@@ -18,5 +21,5 @@ function [ output_image ] = Medium( input_image, filter_size )
            output_image(x-padding,y-padding) = median(window);
        end
     end
-    output_image = uint8(output_image);
+    
 end
--- SelectValue.m
+++ SelectValue.m
@@ -1,11 +1,16 @@
 function [ output_image ] = SelectValue( input_image, value )
+%SELECTVALUE Returns a bianry image of the largest frequency - value
+%element.
 
+    % Sets up the image and finds the frequencyes of the values in the
+    % image.
     [n,m]=size(input_image);
     values = unique(input_image);
     frequency = [values,histc(input_image(:),values)];
     frequency = sortrows(frequency,2);
     value1 = frequency(length(frequency)-value,1);
     
+    % Cycles through the image setting all instances of the value to 1.
     for i=1:n
         for j=1:m
             if input_image(i,j)==value1
@@ -15,6 +20,8 @@ function [ output_image ] = SelectValue( input_image, value )
             end
         end
     end
+    
+    % Converts the output to logical.
     output_image=logical(input_image);
 
 end

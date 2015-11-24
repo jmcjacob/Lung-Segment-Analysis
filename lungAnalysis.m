c7c8e2f14c6dbc8bf9511e53842658e260a70e79
Jacob Carse <jacob.carse@gmail.com>
2015-11-24 23:06:12 +0000

    Added Titles

--- lungAnalysis.m
+++ lungAnalysis.m
@@ -10,14 +10,14 @@ function [ output_image ] = lungAnalysis( input )
     denoised_image = im2bw(i,level);
     denoised_image = Erode(denoised_image,strel('square',3));
     denoised_image = Dilate(denoised_image,strel('square',3));
-    %figure('name','Task 1'), imshow(denoised_image);
+    %figure('name','Task 1'), imshow(denoised_image), title('Task 1');
     
     %Task 2
     lables = CCLabaling(denoised_image);
     hole_image = SelectValue(lables,2) + SelectValue(lables,3);
     dilated_image = Dilate(hole_image,strel('disk',15));
     circle_image = Erode(dilated_image,strel('disk',15));
-    %figure('name', 'Task 2'), imshow(circle_image);
+    %figure('name', 'Task 2'), imshow(circle_image), title('Task 2');
     
     %Task 3
     circle_image = circle_image - hole_image;
@@ -28,9 +28,9 @@ function [ output_image ] = lungAnalysis( input )
     edge_image = EdgeDetect(circle_image);
     edge_image = Dilate(edge_image,strel('disk',1));
     output_image = input_image + Binary2Green(edge_image);
-    figure('name', 'Task 3'), imshow(output_image);
+    figure('name', 'Task 3'), imshow(output_image), title('Task 3');
     
     %Task 4
     figure('name', 'Task 4'),
-    imshow(logical2rgb(circle_image)+Binary2Green(edge_image));
+    imshow(logical2rgb(circle_image)+Binary2Green(edge_image)), title('Task 4');
 end
